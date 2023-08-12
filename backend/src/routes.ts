01bff34f0c2fcce0d6891bc80cf7b8a9cf8a5253
Node <abdoollahikbk@gmail.com>
2023-08-12 01:53:37 +0100

    Integrated Prisma ORM

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,10 @@
-import {Express, Request, Response} from "express"
+import { Express, Request, Response } from "express"
 
 
 function routes(app: Express) {
     app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200))
+
+    app.get("/", (req: Request, res: Response) => res.status(200).send({ message: "Api is running!" }))
 }
 
 export default routes
